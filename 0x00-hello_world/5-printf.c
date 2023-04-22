bb43aacfde9c5152665aeb05bfc66c29e3655064
doshly <doshly92@gmail.com>
2023-04-22 07:51:40 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
+ * main - A program that print a line using the printf f
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    return (0);
-}:
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
