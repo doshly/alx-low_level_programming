f3d8af1911bb82f1797a6f6ab7caff4ced801037
doshly <doshly92@gmail.com>
2023-04-22 14:03:51 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,6 @@ int b;
 long int c;
 long long int d;
 float f;
-
 printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("size of an int: %lu byte(S)\n", (unsigned long)sizeof(b));
 printf("size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
