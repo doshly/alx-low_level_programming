788f13bb3f5621d00b05bee09473fcc143074603
doshly <doshly92@gmail.com>
2023-04-23 08:59:12 -0700

    6=size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - A c program that prints the size of various computer types
- * Return: 0 (Success)
+ * main - A c program that prints the various computer types
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
