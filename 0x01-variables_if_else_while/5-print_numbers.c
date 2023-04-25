2a86aa502fcda6a7fec7eab6347330bda70e0229
doshly <doshly92@gmail.com>
2023-04-25 03:25:22 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
- *main - Entry point
- *Description: print all single digit number of 10 starting from 0
- *Result: Always 0 (success)
+ * main - Entry point
+ * Description: print single digit number of base 10 
+ * Result: Always 0 (success)
  */
 int main(void)
 {
