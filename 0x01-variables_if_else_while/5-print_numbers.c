3e373c5a039faeb842622a5a06afaed807b5a62d
doshly <doshly92@gmail.com>
2023-04-25 03:16:22 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio>
+/**
+ *main - Entry point
+ *Description: print all single digit number of 10 starting from 0
+ *Result: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	print("\n");
+	return (0);
+}
