146952aac816d0c85f23611294550bf787c08d3f
doshly <doshly92@gmail.com>
2023-04-26 02:14:03 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,11 +14,11 @@ for (j = 0; j < 100; j++)
 {
 if (i <= j)
 {
-putchar((i / 10) + 48);
-putchar((i % 10) + 48);
+putchar((I / 10) + 48);
+putchar((I % 10) + 48);
 putchar(' ');
-putchar((j % 10) + 48);
-putchar((j / 10) + 48);
+putchar((J % 10) + 48);
+putchar((J / 10) + 48);
 {
 if (i != 98 || j != 98)
 {
