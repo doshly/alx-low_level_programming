3d80a504aa03c0e07811a7f506a8eec420c6e26b
doshly <doshly92@gmail.com>
2023-04-30 02:41:18 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 i#include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
+* main - Tests function for positive or negative
 * Return: 0
 */
 
@@ -11,6 +11,5 @@ int main(void)
 
         i = 0;
         positive_or_negative(i);
-
-        return (0);
+return (0);
 }
